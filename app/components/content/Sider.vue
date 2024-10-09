129595e1728a534ca6a5bca9a78d44a8ec812b15
pieceook <pieceook@gmail.com>
2024-10-09 21:00:57 +0800

    docs: update layout

--- app/components/content/Sider.vue
+++ app/components/content/Sider.vue
@@ -4,15 +4,13 @@ useScroller('.guide-comp')
 
 <template>
   <div class="fixed top-[90px] h-full max-h-[calc(100%-180px)] max-w-[240px] w-[240px] flex flex-col overflow-x-hidden overflow-y-scroll">
-    <Toc data-allow-mismatch class="toc-comp max-h-[45vh]" />
-    <Guide class="guide-comp max-h-[55vh] overflow-x-hidden overflow-y-scroll" />
+    <Toc data-allow-mismatch class="toc-comp max-h-[45vh] shadow-md" />
+    <Guide class="guide-comp mt-[15px] max-h-[55vh] overflow-x-hidden overflow-y-scroll" />
   </div>
 </template>
 
 <style lang="scss" scoped>
 .guide-comp {
-  @apply mt-[10px];
-
   @include scrollbar-style;
 
   :deep(.sub-item) {
