386f860d452e7087a79e11ef9681095ef0fdd1e0
pieceook <pieceook@gmail.com>
2024-11-10 16:48:55 +0800

    docs: update doc

--- app/components/com/Svgs.vue
+++ app/components/com/Svgs.vue
@@ -1,7 +1,7 @@
 <template>
   <svg aria-hidden="true" class="absolute hidden h-0 w-0">
-    <symbol id="icon_arrow_down" viewBox="0 0 2048 2048">
-      <path fill="currentColor" d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z" />
+    <symbol id="icon_svg_test" viewBox="0 0 24 24">
+      <path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 8h-2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h2v-4h-1M7 8H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3m7-8l1.5 8h1L14 8" />
     </symbol>
   </svg>
 </template>
